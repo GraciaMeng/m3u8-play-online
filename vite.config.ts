b9d7f87d9455ee12f10f33627a8133fd2204b223
MengJiaXi <1040616435@qq.com>
2022-07-24 14:15:05 +0800

    chore: set vite base site url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://graciameng.github.io/m3u8-play-online/',
   plugins: [vue()],
   resolve: {
     alias: {
